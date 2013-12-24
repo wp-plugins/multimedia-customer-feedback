be461893eacf39d4cbf298d136e5fc2a7c402293
grabimo <grabimo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-24 23:57:18 +0000

    tagging version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/multimedia-customer-feedback/tags/1.0.0@828249 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ multimedia-feedback.js
@@ -0,0 +1,268 @@
+if (typeof LETSCAN == 'undefined' || !LETSCAN) {
+	var LETSCAN = {};
+}
+LETSCAN.apps = LETSCAN.apps || {};
+(function() {
+	var defaultConfig = {
+		trigger : null
+	};
+	LETSCAN.apps.Grabimo = function(userConfig) {
+		var that = this;
+		that.UI = {};
+		that._init(userConfig);
+		return {
+			setTrigger : function(el) {
+				that._setTrigger(el);
+			},
+			startFlow : function(alias) {
+				var win = that._render();
+				
+				var url = "http://www.grabimo.com/app/addGig.html?alias=" + alias; 
+				if (win.location) {
+					win.location = url;
+				} else {
+					win.src = url;
+				}
+			},
+			closeFlow : function() {
+				that._destroy();
+			},
+			isOpen : function() {
+				if (that.grabimoWindow) {
+					if (that.grabimoWindow == 'incontext') {
+						return that.isOpen;
+					} else {
+						if (typeof that.grabimoWindow == 'object') {
+							return (!that.grabimoWindow.closed);
+						} else {
+							return false;
+						}
+					}
+				} else {
+					return that.isOpen;
+				}
+			}
+		};
+	};
+	LETSCAN.apps.Grabimo.prototype = {
+		name : 'GrabimoFrame',
+		isOpen : false,
+		grabimoWindow : 'incontext',
+		_init : function(userConfig) {
+			if (userConfig) {
+				for ( var key in defaultConfig) {
+					if (typeof userConfig[key] !== 'undefined') {
+						this[key] = userConfig[key];
+					} else {
+						this[key] = defaultConfig[key];
+					}
+				}
+			}
+			if (this.trigger) {
+				this._setTrigger(this.trigger);
+			}
+		},
+		_launchMask : function() {
+			this._createMask();
+			this._centerLightbox();
+			this._bindEvents();
+		},
+		_render : function() {
+			var ua = navigator.userAgent, win;
+			if (ua.match(/iPhone|iPod|iPad|Android|Blackberry.*WebKit/i)) {
+				win = window.open('', this.name);
+				this.grabimoWindow = win;
+				return win;
+			} else {
+				this._buildDOM();
+				this._launchMask();
+				this.isOpen = true;
+				return this.UI.iframe;
+			}
+		},
+		_buildDOM : function() {
+			this.UI.wrapper = document.createElement('div');
+			this.UI.wrapper.id = this.name;
+			
+			this.UI.panel = document.createElement('div');
+			this.UI.panel.className = 'panel';
+			
+			try {
+				this.UI.iframe = document.createElement('<iframe name="' + this.name + '">');
+			} catch (e) {
+				this.UI.iframe = document.createElement('iframe');
+				this.UI.iframe.name = this.name;
+			}
+			this.UI.iframe.scrolling = 'no';
+			this.UI.iframe.allowTransparency = 'true';
+			
+			this.UI.close = document.createElement('a');
+			this.UI.close.href = "javascript:;";
+			this.UI.close.title = "Close";
+			this.UI.close.id = "close";
+			this.UI.close.onclick = this._destroy;
+
+			this.UI.mask = document.createElement('div');
+			this.UI.mask.className = 'mask';
+			this.UI.panel.appendChild(this.UI.iframe);
+			this.UI.panel.appendChild(this.UI.close);
+			this.UI.wrapper.appendChild(this.UI.mask);
+			this.UI.wrapper.appendChild(this.UI.panel);
+			document.body.appendChild(this.UI.wrapper);
+		},
+		_createMask : function(e) {
+			var windowWidth, windowHeight, scrollWidth, scrollHeight, width, height;
+			var actualWidth = (document.documentElement) ? document.documentElement.clientWidth
+					: window.innerWidth;
+			if (window.innerHeight && window.scrollMaxY) {
+				scrollWidth = actualWidth + window.scrollMaxX;
+				scrollHeight = window.innerHeight + window.scrollMaxY;
+			} else if (document.body.scrollHeight > document.body.offsetHeight) {
+				scrollWidth = document.body.scrollWidth;
+				scrollHeight = document.body.scrollHeight;
+			} else {
+				scrollWidth = document.body.offsetWidth;
+				scrollHeight = document.body.offsetHeight;
+			}
+			if (window.innerHeight) {
+				windowWidth = actualWidth;
+				windowHeight = window.innerHeight;
+			} else if (document.documentElement
+					&& document.documentElement.clientHeight) {
+				windowWidth = document.documentElement.clientWidth;
+				windowHeight = document.documentElement.clientHeight;
+			} else if (document.body) {
+				windowWidth = document.body.clientWidth;
+				windowHeight = document.body.clientHeight;
+			}
+			width = (windowWidth > scrollWidth) ? windowWidth : scrollWidth;
+			height = (windowHeight > scrollHeight) ? windowHeight
+					: scrollHeight;
+			this.UI.mask.style.width = width + 'px';
+			this.UI.mask.style.height = height + 'px';
+		},
+		_centerLightbox : function(e) {
+			var width, height, scrollY;
+			if (window.innerWidth) {
+				width = window.innerWidth;
+				height = window.innerHeight;
+				scrollY = window.pageYOffset;
+			} else if (document.documentElement
+					&& (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+				width = document.documentElement.clientWidth;
+				height = document.documentElement.clientHeight;
+				scrollY = document.documentElement.scrollTop;
+			} else if (document.body
+					&& (document.body.clientWidth || document.body.clientHeight)) {
+				width = document.body.clientWidth;
+				height = document.body.clientHeight;
+				scrollY = document.body.scrollTop;
+			}
+
+			this.UI.panel.style.left = Math
+					.round((width - this.UI.iframe.offsetWidth) / 2)
+					+ 'px';
+			var panelTop = Math
+					.round((height - this.UI.iframe.offsetHeight) / 2)
+					+ scrollY;
+
+			if (panelTop < 5) {
+				panelTop = 10;
+			}
+			this.UI.panel.style.top = panelTop + 'px';
+		},
+		_bindEvents : function() {
+			addEvent(window, 'resize', this._createMask, this);
+			addEvent(window, 'resize', this._centerLightbox, this);
+			addEvent(window, 'unload', this._destroy, this);
+		},
+		_setTrigger : function(el) {
+			if (el.constructor.toString().indexOf('Array') > -1) {
+				for (var i = 0; i < el.length; i++) {
+					this._setTrigger(el[i]);
+				}
+			} else {
+				el = (typeof el == 'string') ? document.getElementById(el) : el;
+				if (el && el.form) {
+					el.form.target = this.name;
+				} else if (el && el.tagName.toLowerCase() == 'a') {
+					el.target = this.name;
+				}
+				addEvent(el, 'click', this._triggerClickEvent, this);
+			}
+		},
+		_triggerClickEvent : function(e) {
+			this._render();
+		},
+		_destroy : function(e) {
+			if (typeof this.grabimoWindow == 'object') {
+				try {
+					this.grabimoWindow.close();
+				} catch (er) {
+				}
+			}
+			if (document.getElementById('GrabimoFrame')) {
+				var parentDiv = document.getElementById('GrabimoFrame').parentNode;
+				parentDiv.removeChild(document.getElementById('GrabimoFrame'));
+			}
+			if (this.isOpen && this.UI.wrapper.parentNode) {
+				this.UI.wrapper.parentNode.removeChild(this.UI.wrapper);
+			}
+			if (this.interval) {
+				clearInterval(this.interval);
+			}
+			removeEvent(window, 'resize', this._createMask);
+			removeEvent(window, 'resize', this._centerLightbox);
+			removeEvent(window, 'unload', this._destroy);
+			removeEvent(window, 'message', this._windowMessageEvent);
+			this.isOpen = false;
+		}
+	};
+
+	// === process events =================================================
+	// add, remove enevts...
+	// ====================================================================
+	var eventCache = [];
+	function addEvent(obj, type, fn, scope) {
+		scope = scope || obj;
+		var wrappedFn;
+		if (obj) {
+			if (obj.addEventListener) {
+				wrappedFn = function(e) {
+					fn.call(scope, e);
+				};
+				obj.addEventListener(type, wrappedFn, false);
+			} else if (obj.attachEvent) {
+				wrappedFn = function() {
+					var e = window.event;
+					e.target = e.target || e.srcElement;
+					e.preventDefault = function() {
+						window.event.returnValue = false;
+					};
+					fn.call(scope, e);
+				};
+				obj.attachEvent('on' + type, wrappedFn);
+			}
+		}
+		eventCache.push([ obj, type, fn, wrappedFn ]);
+	}
+
+	function removeEvent(obj, type, fn) {
+		var wrappedFn, item, len, i;
+		for (i = 0; i < eventCache.length; i++) {
+			item = eventCache[i];
+			if (item[0] == obj && item[1] == type && item[2] == fn) {
+				wrappedFn = item[3];
+				if (wrappedFn) {
+					if (obj.removeEventListener) {
+						obj.removeEventListener(type, wrappedFn, false);
+					} else if (obj.detachEvent) {
+						obj.detachEvent('on' + type, wrappedFn);
+					}
+				}
+			}
+		}
+	}
+}());
+
+var grab_multimedia_feedback = new LETSCAN.apps.Grabimo();
---
+++ multimedia-feedback.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * @Multimedia Customer Feedback
+ */
+/*
+Plugin Name: Multimedia Customer Feedback
+Plugin URI: http://www.grabimo.com/dev/wordpress-plugin.html
+Description: Inspire your customers to provide candid feedback in video, audio, photo, and text formats. Make it easy for you to reward customers for sharing experience, increase customer satisfaction, and improve product or service quality. 
+Version: 1.0.0
+Author: Grabimo
+Author URI: http://www.grabimo.com
+License: GPLv2 or later
+*/
+
+/*  Copyright 2013  Grabimo  (email : admin@grabimo.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// --- add settings -----------------------------------------
+// http://codex.wordpress.org/Settings_API
+// Add a section, field and settings during admin_init
+function multimedia_feedback_settings() {
+	// Add the section to General settings so we can add our fields to it
+	add_settings_section(
+		'multimedia_feedback_setting_section',
+		'Multimedia Customer Feedback',
+		'multimedia_feedback_setting_section_callback_function',
+		'general'
+	);
+	
+	// Add the field with the names and function to use for our new settings, put it in our new section
+	add_settings_field(
+		'multimedia_feedback_business_alias',
+		'Business Alias',
+		'multimedia_feedback_setting_field_callback_function',
+		'general',
+		'multimedia_feedback_setting_section'
+	);
+ 	
+	// Register our setting so that $_POST handling is done for us and
+	// our callback function just has to echo the <input>
+ 	register_setting( 'general', 'multimedia_feedback_business_alias' );
+} 
+add_action( 'admin_init', 'multimedia_feedback_settings' );
+ 
+// section callback function
+function multimedia_feedback_setting_section_callback_function() {
+	//echo '<p>Multimedia Feedback</p>';
+}
+
+// field callback function
+function multimedia_feedback_setting_field_callback_function() {
+	// create the setting field for multimedia customer feedback
+	$value = get_option( 'multimedia_feedback_business_alias', 'example' );
+	$output = '<input type="text" id="multimedia_feedback_business_alias" name="multimedia_feedback_business_alias" value="' . $value . '" /> To create your alias, sign up at <a href="http://www.grabimo.com">http://www.grabimo.com</a>.';
+	if ($value == 'example') {
+		$output = $output . ' The alias, "example", is for demonstration only.';
+	}
+	
+	echo $output;
+}
+
+// --- add javascrit and CSS file on webpage head ------------
+function multimedia_feedback_files() {
+	// Register the script like this for a plugin:
+	wp_register_script('multimedia-feedback-js-file', plugins_url( 'multimedia-feedback.js', __FILE__ ) );
+	// or
+	// Register the script like this for a theme:
+	wp_register_script( 'multimedia-feedback-js-file', get_template_directory_uri() . '/multimedia-feedback.js' );
+
+	// For either a plugin or a theme, you can then enqueue the script:
+	wp_enqueue_script( 'multimedia-feedback-js-file' );
+	
+	// Register the script like this for a plugin:
+	wp_register_style('multimedia-feedback-css-file', plugins_url( 'multimedia-feedback.css', __FILE__ ) );
+	// or
+	// Register the script like this for a theme:
+	wp_register_style( 'multimedia-feedback-css-file', get_template_directory_uri() . '/multimedia-feedback.css' );
+
+	// For either a plugin or a theme, you can then enqueue the script:
+	wp_enqueue_style( 'multimedia-feedback-css-file' );
+}
+add_action( 'wp_enqueue_scripts', 'multimedia_feedback_files' );
+
+// --- create the short code, the Feedback button will dispay on webpage ---
+function multimedia_feedback_short_code() {
+	// retreive business alias from the admin setting page, after registration at http://www.grabimo.com 
+	$businessAlias = get_option( 'multimedia_feedback_business_alias', 'example' );
+    
+	// show the feedback image button, after click it, the iframe lightbox pop up 
+	return "<input type='image' src=\"" . plugins_url( 'multimedia-feedback.png' , __FILE__ ) . "\" id=\"grabimo-feedback\" onclick=\"grab_multimedia_feedback.startFlow('" . $businessAlias . "')\">";
+}
+add_shortcode('grab-multimedia-feedback', 'multimedia_feedback_short_code');   
