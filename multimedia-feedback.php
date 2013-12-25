21aaf76d5359fbc520180f31c6c49cb8092e6e2f
grabimo <grabimo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-25 02:06:12 +0000

    updated screenshots
    git-svn-id: https://plugins.svn.wordpress.org/multimedia-customer-feedback/trunk@828283 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- multimedia-feedback.php
+++ multimedia-feedback.php
@@ -6,7 +6,7 @@
 Plugin Name: Multimedia Customer Feedback
 Plugin URI: http://www.grabimo.com/dev/wordpress-plugin.html
 Description: Inspire your customers to provide candid feedback in video, audio, photo, and text formats. Make it easy for you to reward customers for sharing experience, increase customer satisfaction, and improve product or service quality. 
-Version: 1.0.0
+Version: 1.0.1
 Author: Grabimo
 Author URI: http://www.grabimo.com
 License: GPLv2 or later
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Grabimo
 Tags: Customer Satisfaction, Customer Testimonial, Customer Feedback, Customer Retention, Video Testimonial
 Requires at least: 3.0.1
 Tested up to: 3.2.1
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
